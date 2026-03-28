9f92a0e6f410260b560d73eaca9e22649c665ff0
Melina Ojeda <melina.ojeda@istea.com.ar>
2026-03-28 13:03:53 -0300

    add product card rendering in app.js

--- src/app.js
+++ src/app.js
@@ -1,5 +1,21 @@
 import { getProducts } from "./api.js";
 
+
+let productCard = document.querySelector('#product-card');
+
 getProducts().then((products) => {
-console.log(products);
+let template = '';
+products.forEach((p) => {
+    template += `
+        <div class="card" style="width: 18rem;">
+            <img src="${p.image}" class="card-img-top" alt="${p.title}">
+            <div class="card-body">
+                <h5 class="card-title">${p.title}</h5>
+                <p class="card-text">${p.description}</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>
+    `;
+});
+    productCard.innerHTML += template;
 });
